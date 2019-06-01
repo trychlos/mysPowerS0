31676044fd9ca1aaafccf4c81529f8897a1cd084
Pierre Wieser <pwieser@trychlos.org>
2019-06-01 20:15:39 +0200

    sEeprom: improve dump

--- eeprom.cpp
+++ eeprom.cpp
@@ -16,10 +16,12 @@
 void eepromDump( sEeprom &data )
 {
 #ifdef EEPROM_DEBUG
-    Serial.print( F( "[eepromDump] mark='" )); Serial.print( data.mark ); Serial.println( F( "'" ));
-    Serial.print( F( "[eepromDump] version='" )); Serial.println( data.version );
-    Serial.print( F( "[eepromDump] min_period_ms='" )); Serial.println( data.min_period_ms );
-    Serial.print( F( "[eepromDump] max_period_ms='" )); Serial.println( data.max_period_ms );
+    Serial.print( F( "[eepromDump] mark='" ));          Serial.print( data.mark ); Serial.println( F( "'" ));
+    Serial.print( F( "[eepromDump] version=" ));        Serial.println( data.version );
+    Serial.print( F( "[eepromDump] min_period_ms=" ));  Serial.println( data.min_period_ms );
+    Serial.print( F( "[eepromDump] max_period_ms=" ));  Serial.println( data.max_period_ms );
+    Serial.print( F( "[eepromDump] auto_save_ms=" ));   Serial.println( data.auto_save_ms );
+    Serial.print( F( "[eepromDump] auto_dump_ms=" ));   Serial.println( data.auto_dump_ms );
 #endif
     for( uint8_t i=0 ; i<DEVICE_COUNT ; ++i ){
 #ifdef EEPROM_DEBUG
@@ -34,6 +36,10 @@ void eepromDump( sEeprom &data )
  */
 void eepromRead( sEeprom &data, pEepromRead pfnRead, pEepromWrite pfnWrite )
 {
+#ifdef EEPROM_DEBUG
+    Serial.println( F( "[eepromRead]" ));
+#endif
+
     for( uint8_t i=0 ; i<sizeof( sEeprom ); ++i ){
         (( uint8_t * ) &data )[i] = pfnRead( i );
     }
@@ -41,8 +47,6 @@ void eepromRead( sEeprom &data, pEepromRead pfnRead, pEepromWrite pfnWrite )
     if( data.mark[0] != 'P' || data.mark[1] != 'W' || data.mark[2] != 'I' || data.mark[3] != 0 ){
         eepromReset( data, pfnWrite );
     }
-    // dump the full EEPROM every time
-    eepromDump( data );
 }
 
 /**
@@ -51,27 +55,23 @@ void eepromRead( sEeprom &data, pEepromRead pfnRead, pEepromWrite pfnWrite )
 void eepromReset( sEeprom &data, pEepromWrite pfnWrite )
 {
 #ifdef EEPROM_DEBUG
-    unsigned long start_ms = millis();
-    Serial.print( F( "[eepromReset] begin=" )); Serial.println( start_ms );
+    Serial.println( F( "[eepromReset]" ));
 #endif
+
     memset( &data, '\0', sizeof( sEeprom ));
     strcpy( data.mark, "PWI" );
-    data.version = 1;
+    data.version = EEPROM_VERSION;
 
     data.min_period_ms = 60000;           // at most every minute
     data.max_period_ms = 24*3600000;      // at least once per day
     data.auto_save_ms = 7*24*3600000;     // weekly save
+    data.auto_dump_ms = 86400000;         // daily dump
 
     for( uint8_t i=0 ; i<DEVICE_COUNT ; ++i ){
         deviceReset( &data.device[i] );
     }
+
     eepromWrite( data, pfnWrite );
-#ifdef EEPROM_DEBUG
-    unsigned long end_ms = millis();
-    Serial.print( F( "[eepromReset] rewrite=" )); Serial.print( end_ms );
-    Serial.print( F( ", elapsed=" ));  Serial.print( untilNow( end_ms, start_ms ));
-    Serial.println( F( " ms" ));
-#endif
 }
 
 /**
@@ -79,6 +79,10 @@ void eepromReset( sEeprom &data, pEepromWrite pfnWrite )
  */
 void eepromWrite( sEeprom &data, pEepromWrite pfnWrite )
 {
+#ifdef EEPROM_DEBUG
+    Serial.println( F( "[eepromWrite]" ));
+#endif
+
     for( uint8_t i=0 ; i<sizeof( sEeprom ); ++i ){
         pfnWrite( i, (( uint8_t * ) &data )[i] );
     }
--- eeprom.h
+++ eeprom.h
@@ -9,21 +9,25 @@
  *  This is the data structure saved in the EEPROM.
  * 
  *  MySensors leave us with 256 bytes to save configuration data in the EEPROM :
- *  - the mark takes 4 bytes
- *  - 252/4 = 63 bytes per module
+ *  - the prefix takes 17 bytes
+ *  - 256-17 = 239
+ *  - 239/4 = 59 bytes per module
  * 
  * pwi 2019- 5-26 v1 creation
  */
+
+#define EEPROM_VERSION    2
 
 typedef uint8_t pEepromRead( uint8_t );
 typedef void    pEepromWrite( uint8_t, uint8_t );
 
 typedef struct {
     char          mark[4];
-    uint8_t       version;            // 1
+    uint8_t       version;
     unsigned long min_period_ms;
     unsigned long max_period_ms;
     unsigned long auto_save_ms;
+    unsigned long auto_dump_ms;
     sDevice       device[DEVICE_COUNT];
 }
   sEeprom;
